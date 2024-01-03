4ff6c7d32afbc13075b205654a3fe1bb6a3ef5ab
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-01-03 17:07:32 +0700

    fix: phase1 and phase4

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -8,7 +8,7 @@ const HomeCard: React.FC<CardHomeProps> = ({imageUrl, description})=> {
     const isTablet = useMediaQuery(theme.breakpoints.down("md"));
     return (
         <Paper elevation={15} sx={{ width: isMobile ? "40vw" : isTablet ? "25vw" : "18vw", height: isMobile ? "40vw" : isTablet ? "25vw" : "18vw", backgroundColor: "transparent", display: "flex", flexDirection: "column-reverse", borderRadius: "20px", position:"relative", mt: "40px"}}>
-            <Box component="img" src={imageUrl} style={{position: "absolute",height: "100%", width: "100%", objectFit: "cover", borderTopLeftRadius: "20px", borderTopRightRadius: "20px", paddingBottom: "31%"}} />
+            <Box component="img" src={imageUrl} style={{position: "absolute",height: "100%", width: "100%", objectFit: "cover", borderTopLeftRadius: "20px", borderTopRightRadius: "20px", paddingBottom: "26%"}} />
             <Box component="div" style={{ height: "35%", borderRadius: "20px", backgroundColor: "white", zIndex: 1, justifyContent: "center", display: "flex", flexDirection: "column"}}>
                 <Typography paragraph sx={{textAlign: "center", padding: "10px", fontSize: isTablet ? "0.8rem" : "1.2rem", fontWeight: 600}}>{description}</Typography>
             </Box>
